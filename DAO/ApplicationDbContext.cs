aa823a1f5f3cb711a97f13c6702350d72219ce0a
David Giraldo Lema <david.giraldo52@eia.edu.co>
2026-03-17 23:09:48 -0500

    Feature/DB_Connection (#2)
    * feature: Conectado a Base de Datos en Supabase
    
    * Feature: Conexion a base de datos Supabase
    
    * Fix: No paso nada :)
    
    * No paso nada :)
    
    * feature: Migraciones Listas

---
+++ DAO/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using TutorMatch.Models;
+
+namespace TutorMatch.DAO
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+
+        //Constructor que recibe las opciones de conexión a la bd para tener contexto de esta
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        //listado de clases -> a tablas en la base de datos
+        //Los DB Set nos ayudan a mapear las clases como Entidades
+        //Es decir que Entity Framework lee este archivo para tomar del modelo el esquema de las tablas
+
+        public DbSet<TutorProfile> TutorProfiles { get; set; }
+        public DbSet<Availability> Availabilities { get; set; }
+        public DbSet<Offer> Offers { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Conversation> Conversations { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+
+    }
+}
--- Models/Message.cs
+++ Models/Message.cs
@@ -1,7 +1,30 @@
-﻿namespace TutorMatch.Models
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TutorMatch.Models
 {
     public class Message
     {
+        [Key] [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid MessageId { get; set; }
 
+        [Required]
+        public Guid ConversationId { get; set; }
+
+        [ForeignKey("ConversationId")]
+        public Conversation Conversation { get; set; }
+
+        [Required]
+        public string SenderId { get; set; }
+
+        [ForeignKey("SenderId")]
+        public IdentityUser Sender { get; set; }
+
+        [Required][MaxLength(100)]
+        public string Content { get; set; }
+
+        [Required]
+        public DateTime SentAt { get; set; } = DateTime.UtcNow;
     }
 }
